e96d64fd3829dd03072af194c020c5284fa2b01b
AlekseyShashkovJob <alexeyshashkov@syndicate-g.com>
2026-08-20 00:06:30 +0300

    feat : force portrait

--- lib/myapp.dart
+++ lib/myapp.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
+import 'package:flutter/services.dart';
 import 'screens/main_navigation.dart';
 import 'theme/app_theme.dart';
 
-class RoundTheTableApp extends StatelessWidget {
+class RoundTheTableApp extends StatefulWidget {
   const RoundTheTableApp({super.key});
+
+  @override
+  State<RoundTheTableApp> createState() => _RoundTheTableAppState();
+}
+
+class _RoundTheTableAppState extends State<RoundTheTableApp> {
+  @override
+  void initState() {
+    super.initState();
+    // Форсим портретную ориентацию только для фантика
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+  }
+
+  @override
+  void dispose() {
+    // Возвращаем все ориентации при выходе из фантика
+    // (на случай если core снова захватит управление)
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,20 +60,18 @@ class _AppScrollBehavior extends MaterialScrollBehavior {
     return child;
   }
 
-  // Плавная физика, работает и на Android и на iOS
+  // Плавная физика
   @override
   ScrollPhysics getScrollPhysics(BuildContext context) {
-    return const BouncingScrollPhysics(
-      parent: AlwaysScrollableScrollPhysics(),
-    );
+    return const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics());
   }
 
   // Поддержка всех типов ввода
   @override
   Set<PointerDeviceKind> get dragDevices => {
-        PointerDeviceKind.touch,
-        PointerDeviceKind.mouse,
-        PointerDeviceKind.trackpad,
-        PointerDeviceKind.stylus,
-      };
+    PointerDeviceKind.touch,
+    PointerDeviceKind.mouse,
+    PointerDeviceKind.trackpad,
+    PointerDeviceKind.stylus,
+  };
 }
